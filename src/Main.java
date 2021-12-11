8d831ca82d963afc47727b7354e1c811b4028640
b-whisler <92903702+b-whisler@users.noreply.github.com>
2021-12-10 23:13:55 -0600

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,20 @@
+/**************************************************************
+* Name        : FinalProject
+* Author      : Benjamin Whisler
+* Created     : 10/25/2021
+* Course      : CIS 152 Data Structures
+* Version     : 2.0
+* OS          : Windows 10
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : A restaurant order and menu management system.
+*               Input:  Item information and orders.
+*               Output: Java Swing GUI
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access to
+* my program.         
+***************************************************************/
 import java.util.LinkedList;
 
 import javax.swing.JFrame;
